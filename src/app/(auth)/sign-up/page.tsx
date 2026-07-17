a4e0389c1fd726a4c2deff397999d4c6b85be23b
Om Pathrabe <ompathrabe10@gmail.com>
2026-07-17 21:57:30 +0530

    05-auth-socials

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,12 +1,18 @@
-import { Card } from "@/components/ui/card";
 import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+import {auth} from "@/lib/auth";
+import { redirect } from "next/navigation";
+import {headers} from "next/headers";
 
-const Page = () =>{
-    console.log("SignUp Page rendered");
+const Page = async () =>{
+    const session = await auth.api.getSession({
+        headers: await headers(),
+      });
+    
+      if (!!session) {
+        redirect("/");
+      }
     return (
         <SignUpView/>
     );
 }
 export default Page;
-
-//http://localhost:3000/auth/sign-up
